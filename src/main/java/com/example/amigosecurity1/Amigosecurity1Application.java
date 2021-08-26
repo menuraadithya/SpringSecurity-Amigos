e22216042eb62c9170bf6dff524ed30b66f0d327
emiz98 <64089619+emiz98@users.noreply.github.com>
2021-08-27 01:35:06 +0530

    AntMatchers

---
+++ src/main/java/com/example/amigosecurity1/Amigosecurity1Application.java
@@ -0,0 +1,13 @@
+package com.example.amigosecurity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amigosecurity1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Amigosecurity1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/amigosecurity1/Amigosecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amigosecurity1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Amigosecurity1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
